eaac8f5324c6b2b59a2e40eab88a954a171c4335
Sitouze Aurelien <aurelien.sitouze@gmail.com>
2025-05-12 13:28:50 +0200

    Updates

--- src/components/Layout/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="text-white flex flex-row justify-between items-center px-2 py-2 bg-gray-800">
-      <p>Aurélien SITOUZE</p>
-      <p>@2024 my portefolio</p>
-    </div>
-  );
-}
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import process from "process";
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/Portefolio" : "/",
-  plugins: [react()],
-});
